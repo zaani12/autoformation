67eb5c596062c747168978ede878ab520eae4803
zaani12 <zaani.hamza.solicode@gmail.com>
2023-09-26 16:45:01 +0100

    Namespaces

---
+++ namespace.php
@@ -0,0 +1,26 @@
+<?php
+namespace html;
+class Table {
+    public table {
+        public $title ="";
+        public $numRows = 0;
+        public function message(){
+            echo "<p> table'{$this->title}' has {$this->numRows} rows. </p>"
+        }
+    }
+}
+$table = new table();
+$table->titel ="my table";
+$table-> = 5;
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$table->message();
+?>
+
+</body>
+</html>
